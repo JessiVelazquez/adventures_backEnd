cfc82c6897dee7833cd5cc8200284724f25e34e2
Jessi <jessivelazq1@gmail.com>
2021-07-22 21:14:38 -0700

    server running + DB connection

---
+++ modules/data.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const userModel = require('../models/User');
+const superagent = require('superagent');
+
+const Data = {};
+
+//======API ROUTES=====================
+
+
+module.exports = Data;
